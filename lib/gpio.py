ae4122660fa7df41cc187b48caca18a0070fd438
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-03-30 00:10:32 +0900

    [add] lib/gpio.py

---
+++ lib/gpio.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+def gpio_write(pin, value):
+    """
+    update gpio state
+
+    Parameters
+    ----------
+    pin : int
+        target gpio (BCM)
+    value : int
+        next state of target gpio
+    """
+    cmd = 'gpio -g mode {0} out && gpio -g write {0} {1}'.format(pin, value)
+    os.system(cmd)
+
+def gpio_read(pin):
+    """
+    get gpio state
+
+    Parameters
+    ----------
+    pin : int
+        target gpio (BCM)
+
+    Returns
+    -------
+    bool
+        Is target gpio turned on ?
+    """
+    cmd = 'gpio -g read {pin}'.format(pin = pin)
+    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    gpio_state = proc.stdout.readline().decode().rstrip()
+    return gpio_state == '1'
+
