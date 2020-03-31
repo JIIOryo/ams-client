99b82e538b1dd5bc28d5d485c6f6bb4161b2035d
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-03-31 22:10:03 +0900

    [add] ams_client.py

--- ams_client.py
+++ ams_client.py
@@ -1,1 +1,22 @@
+import os
+import subprocess
+import time
 
+from lib.config import get_config_item
+
+PWD = os.getcwd()
+# SENSOR_MANAGER_PATH = '/'.join([PWD, ..., ])
+SUBSCRIBER_PATH = '/'.join([PWD, 'subscriber', 'subscriber.py'])
+
+def main():
+    # check cron to write gpio 
+
+    # open sensor manager
+
+    # open subscriber
+    network_connection_wait_time = get_config_item(NETWORK_CONNECTION_WAIT_TIME)
+    time.sleep(network_connection_wait_time)
+    subprocess.Popen(['python3', SUBSCRIBER_PATH])
+
+if __name__ == '__main__':
+    main()
