f845de3c6c1cf2f100ec96d5fc454e55d392dcbe
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-04-12 22:42:21 +0900

    [add] server port setting

--- server/app.py
+++ server/app.py
@@ -1,11 +1,20 @@
-from flask import flask
+import sys
 
+from flask import Flask
+
+import pathlib
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append( str(current_dir) + '/../' )
+
+from lib.config import get_config_item
 
 app = Flask(__name__)
+
+server_config = get_config_item('LOCAL_SERVER')
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3321)
+    app.run(debug=True, host='0.0.0.0', port=server_config['PORT'])
