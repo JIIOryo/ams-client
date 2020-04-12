b41b594b92c5acbb6bb7a5b3512fd0ed3f2c54e9
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-04-12 23:42:27 +0900

    [add] check device state in local web client

--- server/app.py
+++ server/app.py
@@ -6,14 +6,17 @@ import pathlib
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append( str(current_dir) + '/../' )
 
-from lib.config import get_config_item
+from lib.config import get_config_item, get_gpio_config
+from service.device import get_all_device_state
 
 app = Flask(__name__)
 server_config = get_config_item('LOCAL_SERVER')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    devices = get_gpio_config()
+    device_state = get_all_device_state()
+    return render_template('index.html', devices=devices, device_state=device_state)
 
 
 if __name__ == '__main__':
