b8214e02e8f029dad4c2a2a0a2f0d1f83ac37ad8
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-04-26 01:19:53 +0900

    [add] device_control api

--- server/app.py
+++ server/app.py
@@ -1,22 +1,32 @@
+import json
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import pathlib
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append( str(current_dir) + '/../' )
 
 from lib.config import get_config_item, get_gpio_config
+from on_message.device_control import device_control
 from service.device import get_all_device_state
 
 app = Flask(__name__)
 server_config = get_config_item('LOCAL_SERVER')
+
+empty_response = json.dumps({})
 
 @app.route('/')
 def index():
     devices = get_gpio_config()
     device_state = get_all_device_state()
     return render_template('index.html', devices=devices, device_state=device_state)
+
+@app.route('/device/control', methods=['POST'])
+def device_control_():
+    print(request.json)
+    device_control(message = request.data)
+    return empty_response
 
 
 if __name__ == '__main__':
