04213eccd7da76b9b14e8638739a2a2ceb6aeb41
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-07-03 02:05:33 +0900

    [add] logger slack post

--- service/logger.py
+++ service/logger.py
@@ -5,8 +5,12 @@ import pathlib
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append( str(current_dir) + '/../' )
 
+from commons.consts import (
+    SLACK_NOTIFICATION_TYPE,
+)
 from lib.config import get_config_item
 from lib.color import Color, color_text
+from lib.notification import post_log_to_slack
 
 DEBUG = "DEBUG"
 INFO = "INFO"
@@ -17,23 +21,28 @@ FATAL = "FATAL"
 LOG_LEVEL = {
     "DEBUG": {
         "LEVEL": 1,
-        "COLOR": Color.CYAN
+        "COLOR": Color.CYAN,
+        "SLACK_COLOR": '#00FFFF',
     },
     "INFO": {
         "LEVEL": 2,
-        "COLOR": Color.GREEN
+        "COLOR": Color.GREEN,
+        "SLACK_COLOR": '#01DF01',
     },
     "WARN": {
         "LEVEL": 3,
-        "COLOR": Color.YELLOW
+        "COLOR": Color.YELLOW,
+        "SLACK_COLOR": '#D7DF01',
     },
     "ERROR": {
         "LEVEL": 4,
-        "COLOR": Color.RED
+        "COLOR": Color.RED,
+        "SLACK_COLOR": '#FF4000',
     },
     "FATAL": {
         "LEVEL": 5,
-        "COLOR": Color.PURPLE
+        "COLOR": Color.PURPLE,
+        "SLACK_COLOR": '#FF00BF',
     },
 }
 MIN_FILE_OUTPUT_LEVEL = 2
@@ -83,7 +92,7 @@ def output_log_file(text: str):
     return
 
 # logger
-def logger(log_level: str, message: str) -> None:
+def logger(log_level: str, message: str, require_slack: bool = False) -> None:
 
     prefix = log_message_prefix_generator(log_level)
 
@@ -99,6 +108,17 @@ def logger(log_level: str, message: str) -> None:
             text = f'{prefix} {message}\n'
         )
     
-    # TODO slack message post
+    # slack
+    if require_slack:
+        mention = ''
+        if log_level == ERROR or log_level == FATAL:
+            mention = '<!channel>'
+        post_log_to_slack(
+            pretext = mention,
+            color = LOG_LEVEL[log_level]['SLACK_COLOR'],
+            title = f'[{log_level}]',
+            text = message,
+            footer = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        )
 
     return
