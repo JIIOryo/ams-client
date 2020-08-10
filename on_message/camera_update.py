0c1aa18dd9ece0c9c68bce67184ae3a048c45073
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2020-08-10 17:54:29 +0900

    [add] on_message update camera

---
+++ on_message/camera_update.py
@@ -0,0 +1,39 @@
+import json
+import sys
+
+import pathlib
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append( str(current_dir) + '/../' )
+
+from service.camera import update_camera
+
+"""
+# message 
+type: json str
+-----
+{
+    "camera_id": "camera_id"
+    "name": "new camera",
+    "camera_device_id": 1,
+    "resolution": {
+        "x": 1700,
+        "y": 1024
+    },
+    "trimming": {
+        "top": 100,
+        "bottom": 1024,
+        "left": 720,
+        "right": 1700
+    }
+}
+"""
+def camera_update(message: str) -> None:
+    updated_camera = json.loads(message)
+    update_camera(
+        camera_id = updated_camera['camera_id'],
+        name = updated_camera['name'],
+        camera_device_id = updated_camera['camera_device_id'],
+        resolution = updated_camera['resolution'],
+        trimming = updated_camera['trimming']
+    )
+    return
